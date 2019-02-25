b9496fd7e18cc9b0b36677168b01062606f33c8c
samaonline <samaonline@live.com>
2019-02-25 00:53:03 -0800

    fix w1w2

--- solver.py
+++ solver.py
@@ -16,7 +16,7 @@ from logger import Logger
 #trainloader = dataloader.getLoader("./mnistPC/train.hdf5", 80, 'train')
 
 #optim = torch.optim.SGD(model.parameters(), lr=1e-6)
-    
+
 def eval(test_iterator, model, grid, sigma):
     acc_all = []
     for (inputs, labels) in (test_iterator):
@@ -26,7 +26,7 @@ def eval(test_iterator, model, grid, sigma):
         pred = torch.argmax(pred, dim=-1)
         acc_all.append(np.mean(pred.detach().cpu().numpy() == labels.numpy()))
     return np.mean(acc_all)
-        
+
 
 
 def train(train_data_dir, test_data_dir, train_iter, log_interval, grid, sigma, batch_size, log_dir):
@@ -38,9 +38,9 @@ def train(train_data_dir, test_data_dir, train_iter, log_interval, grid, sigma, 
     optim = torch.optim.Adam(model.parameters(), lr=1e-2)
     test_iterator = utils.load_data(test_data_dir, batch_size=10)
     train_iterator = utils.load_data(train_data_dir, batch_size=batch_size)
-    cls_criterion = torch.nn.CrossEntropyLoss().cuda()
     for epoch in range(train_iter):  # loop over the dataset multiple times
         running_loss = []
+        cls_criterion = torch.nn.CrossEntropyLoss().cuda()
         for i, (inputs, labels) in enumerate(train_iterator):
             # get the inputs
             #inputs, labels = data
@@ -56,18 +56,17 @@ def train(train_data_dir, test_data_dir, train_iter, log_interval, grid, sigma, 
             loss = cls_criterion(outputs, labels.squeeze())
             loss.backward(retain_graph=True)
             optim.step()
-            # file = open("log.txt","w+") 
-            # file.write(str(loss.item())) 
-            # file.close() 
+            # file = open("log.txt","w+")
+            # file.write(str(loss.item()))
+            # file.close()
             # print statistics
             running_loss.append( loss.item() )
             # if i % log_interval == 0:
-            #     print(np.mean(running_loss))
-                
-            #     file = open("log.txt","w+") 
-            #     file.write(str(loss.item())) 
-            #     file.close() 
             print(np.mean(running_loss))
+
+            #     file = open("log.txt","w+")
+            #     file.write(str(loss.item()))
+            #     file.close()
 
         #print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / (i+1)))
         print("Epoch"+str(epoch))
@@ -89,7 +88,7 @@ if __name__ == '__main__':
     parser.add_argument('--grid', default=10 , type=int, metavar='N', help='grid of sdt')
     parser.add_argument('--sigma', default=0.01 , type=float, metavar='N', help='sigma of sdt')
     parser.add_argument('--log_dir', default="./log_dir" , type=str, metavar='N', help='directory for logging')
-    
+
     args = parser.parse_args()
     test_data_dir = os.path.join(args.data_path, "test.hdf5")
     train_data_dir = os.path.join(args.data_path, "train.hdf5")
