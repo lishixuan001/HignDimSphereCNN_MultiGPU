3aa51c69f17ba447edab5336c99b5e799b868e7f
Yifei Xing <xingyifei2016@berkeley.edu>
2019-03-01 22:58:28 -0800

    S

--- wFM.py
+++ wFM.py
@@ -63,8 +63,8 @@ class wFMLayer(nn.Module):
         idx = idx.view((B, 1, 1)) #reshape to be added to knn indices
         if adj_mtr is None or self.down_sample != 1:
             adj_mtr=pairwise_distance(input_set)
-            print(adj_mtr[0])
         k2 = knn(adj_mtr, k=k, include_myself=True) #B*N*k
+        print(k2[0][0])
         k2 = torch.Tensor(k2).long()+idx
         ptcld = input_set.view(B*N, D, C) #reshape pointset to BN * DC
         ptcld = ptcld.view(B*N, D*C)
